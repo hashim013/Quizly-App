429750e987e17baea582ea24ac03b5459255a40c
Muhammad Hashim <muhammadhashim200229@gmail.com>
2026-02-11 18:45:40 +0500

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quizly_app/main.dart';
 import 'package:quizly_app/quiz.dart';
 
 void main() {
